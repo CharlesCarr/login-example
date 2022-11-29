df72e247d50fafba31aa1a06470cab8b6a86c0e4
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2022-11-29 09:30:30 -0500

    addform component

---
+++ src/components/Form.jsx
@@ -0,0 +1,34 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const Form = () => {
+  const schema = yup.object().shape({
+    email: yup.string().email("Invalid Email!").required("Email required!"),
+    password: yup.string().min(4).max(20).required("Password required!"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="Email" {...register("email")} />
+      <p>{errors.email?.message}</p>
+      <input type="password" placeholder="Password" {...register("password")} />
+      <p>{errors.password?.message}</p>
+      <input type="submit" />
+    </form>
+  );
+};
+
+export default Form;
